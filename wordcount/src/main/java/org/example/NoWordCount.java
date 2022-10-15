8e25059a1f125944799fd456b93e6c769278000d
Jin Young Park <dominicjin@Jins-MacBook-Pro.local>
2022-10-14 17:02:18 -0700

    Implement word count program using MapReduce and exlore partitioners and compare combiners to the in-mapper aggregation design pattern.

---
+++ wordcount/src/main/java/org/example/NoWordCount.java
@@ -0,0 +1,88 @@
+package org.example;
+
+import java.io.IOException;
+import java.util.*;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Partitioner;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+
+public class NoWordCount {
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            String curr = itr.nextToken();
+            int first = curr.charAt(0);
+            if (first == 'm' || first == 'n' || first == 'o' || first == 'p' || first == 'q' ||
+                    first == 'M' || first == 'N' || first == 'O' || first == 'P' || first == 'Q') {
+                word.set(curr);
+                context.write(word, one);
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text, IntWritable, Text, IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    // Custom partitioner
+    public static class CustomPartitioner extends Partitioner<Text,IntWritable>{
+        @Override
+        public int getPartition(Text text, IntWritable intWritable, int i) {
+            String partitionKey = text.toString().toLowerCase();
+            if (partitionKey.startsWith("m")) {
+                return 0;
+            } else if (partitionKey.startsWith("n")) {
+                return 1;
+            } else if (partitionKey.startsWith("o")) {
+                return 2;
+            } else if (partitionKey.startsWith("p")) {
+                return 3;
+            } else if (partitionKey.startsWith("q")) {
+                return 4;
+            }
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "wordCount");
+        job.setJarByClass(NoWordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        //job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        job.setPartitionerClass(CustomPartitioner.class);
+        job.setNumReduceTasks(5);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
